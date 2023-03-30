dca8d8b2e60e0e1002063713846b044c0245259e
Dylan201100 <Lampro123@@>
2023-03-30 17:18:14 +0700

    update cors origin cross

--- server/server.js
+++ server/server.js
@@ -48,7 +48,7 @@ app.get('/', (req, res) => {
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization,Origin, X-Requested-With, Accept');
   next();
 });
 
