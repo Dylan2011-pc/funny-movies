789926092c7c225ebcbb772f222097ae0068c23f
Dylan201100 <Lampro123@@>
2023-03-30 18:22:09 +0700

    cors set origin header

--- server/server.js
+++ server/server.js
@@ -47,7 +47,7 @@ app.get('/', (req, res) => {
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization,Origin, X-Requested-With, Accept');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept');
   next();
 });
 
