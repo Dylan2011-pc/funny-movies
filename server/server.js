4fc9422ecacb5d063b563b3dbda9d9e5f2424381
Dylan201100 <Lampro123@@>
2023-04-05 22:23:31 +0700

    cors config

--- server/server.js
+++ server/server.js
@@ -47,13 +47,13 @@ app.get('/', (req, res) => {
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Origin, X-Requested-With, Accept');
   res.setHeader('Cache-Control', 'no-cache');
   next();
 });
 
 app.use(cors({
-  origin: ['https://funny-movies-7dsu.vercel.app','https://funny-movies-7dsu.vercel.app/*' ]
+  origin: ['https://huongandi.vercel.app','https://huongandi.vercel.app/*' ]
 }));
 
 
